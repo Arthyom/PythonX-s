6e02a213323b1f111cbb7d9688eb6bd4271c82c3
arthyom <a.gonzalezgavina@ugto.mx>
2019-02-05 23:46:41 -0600

    mejorando algo

--- wflsNumerics.py
+++ wflsNumerics.py
@@ -8,6 +8,15 @@ class WflsNumerics(object):
         if expression != 0:
             self.expression = sympify(expression)
             
+    ### calculate fraction factors
+    def fractions(self, lastTerm, firstTerm):
+        a = []
+        for fl in lastTerm:
+            for ft in firstTerm:
+                s = (1.0*fl)/ft
+                if s not in lastTerm and s not in firstTerm: a.append(s)
+        return a 
+
     ### calculate cofficients of a number
     def factorsOf (self, number ):
         factors = []
@@ -24,25 +33,32 @@ class WflsNumerics(object):
         pows = map(int, re.findall(r"[x^](\d+)", expression.replace(' ', '')))
         fact1Er = self.factorsOf(coeffs[0])
         fact2Do = self.factorsOf(coeffs[-1])
-        factFraccion = []
+        factFraccion = self.fractions(fact2Do, fact1Er) ###[ fu/fp for fu in fact2Do for fp in fact1Er ]
+        factorOptions = [fact2Do,fact1Er,factFraccion]
 
         ### iterate while not find total roots
         while roots < max(pows):
-            ### iterate for all factors of the last coefficients
-            for fi in fact2Do:
-                ### iterate for all coeficients 
-                newCoeffs = []
-                for i, ci in enumerate(coeffs):
-                    if i == 0:
-                        newCoeffs.append(coeffs[0])
-                    else:
-                        ### is the second or more iteration
-                        newCoeffs.append( (fi * newCoeffs[i-1])+coeffs[i] )
-                ### if the last number is 0, store the factor that produces it
-                if newCoeffs[-1] == 0:
-                    coeffs = newCoeffs
-                    listRoots.append(fi)
-                    roots += 1
+            ### iterate for each factor from one of three factors options
+            for factN in factorOptions:
+                ### iterate for all factors of the last coefficients
+                for fi in factN:
+                ### iterate for all coeficients
+                    newCoeffs = []
+                    for i, ci in enumerate(coeffs):
+                        if i == 0:
+                            newCoeffs.append(coeffs[0])
+                        else:
+                            ### is the second or more iteration
+                            newCoeffs.append((fi * newCoeffs[i-1])+coeffs[i])
+                    ### if the last number is 0, store the factor that produces it
+                    if newCoeffs[-1] == 0:
+                        coeffs = newCoeffs
+                        listRoots.append(fi)
+                        roots += 1
+                factorOptions.remove(factN)
+            if len(factorOptions) == 0: 
+                listRoots.append('raices complejas')
+                break
         return listRoots
 
     def plotIndependetExpression(self, expression, fromm, to):
@@ -52,9 +68,10 @@ class WflsNumerics(object):
   
 ## +1x^4+0x^3-10x^2+0x+9
 ## +1x^4+1x^3-19x^2+11x+30
-#c = WflsNumerics()
+## +2x^4+1x^3-8x^2-1x+6
+## +4x^4+9x^3-5x^2+9x-9
+c = WflsNumerics()
 
-#s =  c.hornerWfls("+1x^4+1x^3-19x^2+11x+30")
-
-#print  
-#print  c.plotIndependetExpression("+1*x^4+1*x^3-19*x^2+11*x+30", min(s), -min(s) )
+##s =  c.hornerWfls("+4*x^4+9*x^3-5*x^2+9*x-9")
+##print s
+#c.plotIndependetExpression("+4*x^4+9*x^3-5*x^2+9*x-9", min(s), -min(s) )
